45fea554081ef3104af2b870bb1052255956e431
Tom Maher <tmaher@microsoft.com>
2024-05-27 15:00:00 +0100

    code updates

--- app_config.py
+++ app_config.py
@@ -5,6 +5,9 @@ AUTHORITY = "https://login.microsoftonline.com/<<tenantId>>"
 # Application (client) ID of app registration
 CLIENT_ID = "<<clientId>>"
 
+# Application (client) secret of app registration
+CLIENT_SECRET = "<<clientSecret>>"
+
 REDIRECT_PATH = "/getAToken"  # Used for forming an absolute URL to your redirect URI.
 # The absolute URL must match the redirect URI you set
 # in the app's registration in the Azure portal.
